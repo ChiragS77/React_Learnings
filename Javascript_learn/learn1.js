4bf3e853a71524fc339a6688c0f445ce7ee1e21a
Chirag <deshmukhchirag178@gmail.com>
2026-06-06 23:18:20 +0530

    firstcommit

---
+++ Javascript_learn/learn1.js
@@ -0,0 +1,142 @@
+
+function greet(){
+    console.log("Hello world..");
+}
+
+greet();
+
+//using parameter
+function add( a , b){
+    return a + b;
+}
+
+console.log(add(2,5));
+
+//using default value
+
+function myname(name= "guest"){
+    return name;
+}
+
+console.log("chirag")
+
+
+const multiply = (a ,b) =>{
+    return a * b;
+}   
+
+console.log(multiply(5,5));
+
+//setInterval
+
+setInterval(function(){
+    console.log("executed");
+    
+},2000);
+
+// ------------------ Arrays ------------------
+let name =["chirag","rahul","pratham","sohan"];
+
+let fruits = ["apple","orange","pineapple","mango"];
+
+console.log(fruits);
+console.log(fruits[0]);
+
+//add element at last
+fruits.push("Watermelon");
+console.log(fruits);
+
+//remove element at last
+fruits.pop();
+console.log(fruits);
+
+
+//add element at begining
+fruits.unshift("Blueberry");
+console.log(fruits);
+
+//remove first element
+fruits.shift();
+console.log(fruits);
+
+//array length
+console.log(fruits.length);
+
+// for(let i =0; i<fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+
+//------- using forEach method ------------⭐⭐
+fruits.forEach(f=>console.log(f));
+
+//Array of Object
+
+let users = [
+    {id:1,name:"chirag"},
+    {id:2,name:"rahul"},
+    {id:3,name:"shaswat"}
+]
+
+console.log(users[0].name);
+
+users.forEach(user =>{
+    console.log(user.name);
+});
+
+
+// ---------------------- MAP,FILTER,FIND METHODS  ----------------
+
+let numbers = [10,20,30,40,50];
+//--- map method --- transform data based on some condition
+let result = numbers.map(num =>num *2);
+console.log(result);
+console.log(numbers);
+
+// --- filter method --- filters data base on condition
+console.log(numbers.filter(num => num >25));
+
+// -----find() ---------- return the first element that match condition
+console.log(numbers.find(num => num>30));
+
+
+// ---------------------- Objects in Javascript ----------------------
+
+let user ={
+    name:"Chirag Shyam Deshmukh",
+    age:24,
+    city:"japan"
+}
+//access object
+console.log(user.name);
+
+user.age= 25;
+console.log(user);
+
+//add property in object
+user.email = "deshmukhchirag178@gmail.com"
+console.log(user);
+
+//delete property from object
+delete user.city;
+
+
+//Object with function 
+let school = {
+    name:"Ishwar Deshmukh English Medium School, Digras",
+
+   schoolName(){
+    return this.name;
+   }
+};
+
+console.log(school.schoolName())
+
+
+//__________loop in object___________
+// for in loop
+for(let key in user){
+        console.log(key,user[key]);
+}
+
+
